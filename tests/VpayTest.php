d2fc830b0987c89f0edc7424bc1de2e6a98d3cd1
ankio <admin@ankio.net>
2023-05-17 10:53:44 +0800

    init:composer

--- tests/VpayTest.php
+++ tests/VpayTest.php
@@ -5,42 +5,43 @@
 
 namespace tests\tests;
 
+
 use Ankio\PayConfig;
 use Ankio\Vpay;
+use Ankio\objects\PayCreateObject;
 use PHPUnit\Framework\TestCase;
 use function PHPUnit\Framework\assertEquals;
 
 class VpayTest extends TestCase
 {
 
-
+    private PayConfig $config ;
     public function test__construct()
     {
-        $this->testCreate();
-    }
-
-    public function testCreate()
-    {
         include_once "../src/autoload.php";
-        $config = new PayConfig([
+        $this->config = new PayConfig([
             'host'=>'https://pay.dev-ankio.net',
             'key'=>'PrplYUFtiPyKRzqvJGjeQwn4IoHElaz0',
             'time'=>5,
             'id'=>3
         ]);
-        $params = [
-            'pay_type'=>'1',
-            'app_item'=>"测试商品",
-            'notify_url'=>'https://pay.dev-ankio.net',
-            'return_url'=>'https://pay.dev-ankio.net',
-            'price'=>0.02,
-            'param'=>json_encode([
-                'xxxxx'=>'xxxx','xxxxxxxx'=>'xxxxx'
-            ]),
-            'api'=>true,
-        ];
-        $pay  = new Vpay($config);
-        $result = $pay->Create($params);
+        $this->testCreate();
+    }
+
+    public function testCreate()
+    {
+        $order = new PayCreateObject();
+        $order->app_item = "测试商品";
+        $order->appid = $this->config->id;
+        $order->param = json_encode([
+            'xxxxx'=>'xxxx','xxxxxxxx'=>'xxxxx'
+        ]);
+        $order->price = 2.2;
+        $order->pay_type = Vpay::PAY_ALIPAY;
+        $order->notify_url = 'https://pay.dev-ankio.net';
+        $order->return_url = 'https://pay.dev-ankio.net';
+        $pay  = new Vpay($this->config);
+        $result = $pay->Create($order);
 
 
         assertEquals("",$pay->getError());
